f1129ccd33f60910958c426004029c385ea4ef0b
Masud8851 <masud1971.cse@gmail.com>
2021-09-25 15:22:31 +0600

    Meal Db React

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../../images/logo-small.png'
 import '../Header/Header.css'
+import mealIcon from '../../images/meal-icon.png'
 
 const Header = () => {
     return (
@@ -12,6 +13,21 @@ const Header = () => {
                     <a href="/api">API</a>
                     <a href="/forum">Forum</a>
                     <input type="text" placeholder="Search"/>
+                </div>
+            </div>
+            <div className="welcome-section">
+                <div>
+                    <img src={mealIcon} alt="" />
+                </div>
+                <div>
+                    <h2>Welcome to TheMealDB</h2>
+                    <p>Welcome to TheMealDB: An open, crowd-sourced database of Recipes from around the world.
+                        We also offer a free JSON API for anyone wanting to use it.
+                        If you like the site, please consider supporting us on Patreon by clicking the link below...
+                    </p>
+                </div>
+                <div>
+                    <img src={mealIcon} alt="" />
                 </div>
             </div>
         </div>
